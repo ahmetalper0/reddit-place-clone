5d6aca5dd42b5dfb8414689d64f8dfd12b603ef2
Ahmet Alper <ahmetalpererdogan@gmail.com>
2024-08-10 23:36:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var websocket_url = 'https://ahmetalper-reddit-place-clone.hf.space'
+var websocket_reconnect_timeout = 5
+var websocket;
+
 const cell_size = 10;
 const row_size = 300;
 const column_size = 300;
@@ -8,16 +12,76 @@ const grid_height = (column_size * cell_size) + (column_size + 1);
 const canvas = document.querySelector('.grid');
 const context = canvas.getContext('2d');
 
+var grid = [];
+
 canvas.width = grid_width;
 canvas.height = grid_height;
+
+function draw_grid() {
+
+    for (let y = 1; y <= ((column_size - 1) * (cell_size + 1)) + 1; y += cell_size + 1) {
+
+        for (let x = 1; x <= ((row_size - 1) * (cell_size + 1)) + 1; x += cell_size + 1) {
+
+            grid.push({'x' : x, 'y' : y, 'width' : cell_size, 'height' : cell_size, 'color' : '#181818'});
+
+        }
+
+    }
+
+    context.fillStyle = '#252525';
+
+    context.fillRect(0, 0, grid_width, grid_height);
+
+    grid.forEach(cell => {
+
+        context.fillStyle = cell['color'];
+
+        context.fillRect(cell['x'], cell['y'], cell['width'], cell['height']);
+
+    });
+
+}
+
+function update_grid(index, color) {
+
+    cell = grid[index];
+
+    cell['color'] = color;
+
+    context.fillStyle = color;
+
+    context.fillRect(cell['x'], cell['y'], cell['width'], cell['height']);
+
+}
+
+canvas.addEventListener('click', (event) => {
+
+    const rect = canvas.getBoundingClientRect();
+
+    const canvas_x = event.clientX - rect.left;
+    const canvas_y = event.clientY - rect.top;
+
+    grid.forEach((cell, index) => {
+
+        if (canvas_x >= cell['x'] && canvas_x < cell['x'] + cell['width'] &&
+            canvas_y >= cell['y'] && canvas_y < cell['y'] + cell['height']) {
+
+            update_grid(index, selected_color);
+
+            send_message({'message' : 'update color', 'index' : index, 'color' : selected_color});
+
+        }
+
+    });
+
+});
 
 const color_picker = document.querySelector('.color-picker');
 const color_picker_input = document.querySelector('.color-picker-input');
 const eraser = document.querySelector('.eraser');
 
-const colors = ["#ff0000", "#008000", "#0000ff", "#ffc0cb", "#800080", "#ffff00", "#ffa500", "#000000", "#ffffff"];
-
-var selected_color = colors[0];
+var selected_color = '#ff0000';
 
 color_picker.style.backgroundColor = selected_color;
 
@@ -37,108 +101,93 @@ eraser.addEventListener('click', () => {
 
 });
 
-var grid = [];
+function connect_websocket() {
 
-function create_grid() { 
+    websocket = new WebSocket(websocket_url);
 
-    for (let y = 1; y <= ((column_size - 1) * (cell_size + 1)) + 1; y += cell_size + 1) {
+    websocket.onopen = function() {
 
-        for (let x = 1; x <= ((row_size - 1) * (cell_size + 1)) + 1; x += cell_size + 1) {
+        console.log('Connected to WebSocket server');
 
-            grid.push({'x' : x, 'y' : y, 'width' : cell_size, 'height' : cell_size, 'color' : '#181818'});
+        send_message({'message' : 'fetch colors'});
+
+    };
+
+    websocket.onclose = function() {
+
+        console.log('WebSocket connection closed');
+
+        console.log(`Trying to reconnect in ${websocket_reconnect_timeout} seconds`);
+
+        setTimeout(connect_websocket, websocket_reconnect_timeout * 1000);
+
+    };
+
+    websocket.onerror = function(event) {
+
+        console.log(`WebSocket Error: ${event.message || 'Unknown error'}`);
+
+        if (websocket.readyState === WebSocket.OPEN) {
+
+            websocket.close();
 
         }
+
+    };
+
+    websocket.onmessage = function(event) {
+
+        try {
+
+            const data = JSON.parse(event.data);
+
+            if (data['message'] == 'fetch colors') {
+
+                grid_colors = data['colors'];
+
+                grid_colors.forEach(color => {
+
+                    update_grid(color[0], color[1]);
+                    
+                });
+
+            }
+
+            if (data['message'] == 'update color') {
+
+                grid_colors = data['colors'];
+
+                grid_colors.forEach(color => {
+
+                    update_grid(color[0], color[1]);
+                    
+                });
+
+            }
+
+        } catch (error) {
+
+            console.error('Error while parsing message data :', error);
+
+        }
+
+    };
+   
+}
+
+function send_message(message) {
+
+    if (websocket.readyState === WebSocket.OPEN) {
+
+        websocket.send(JSON.stringify(message));
+
+    } else {
+
+        console.log('Message cannot sent ! | WebSocket connection is not open.');
 
     }
-
 }
-
-function update_grid() {
-
-    fetch('https://ahmetalper-reddit-place-clone.hf.space/colors')
-
-        .then(response => response.json())
-
-		.then(data => {
-
-			grid.forEach((cell, index) => {
-
-				cell['color'] = data['colors'][index];
-				
-			});
-
-			draw_grid();
-
-			console.log(`${new Date().toLocaleString()} | Grid has been updated successfully.`);
-
-		})
-
-		.catch(error => {
-
-			console.error(`${new Date().toLocaleString()} | error : ${error}`);
-
-		});
-
-}
-
-function draw_grid() {
-
-    context.fillStyle = '#252525';
-
-    context.fillRect(0, 0, grid_width, grid_height);
-
-    grid.forEach(cell => {
-
-        context.fillStyle = cell['color'];
-
-        context.fillRect(cell['x'], cell['y'], cell['width'], cell['height']);
-
-    });
-
-}
-
-canvas.addEventListener('click', (event) => {
-
-    const rect = canvas.getBoundingClientRect();
-
-    const canvas_x = event.clientX - rect.left;
-    const canvas_y = event.clientY - rect.top;
-
-    grid.forEach((cell, index) => {
-
-        if (canvas_x >= cell['x'] && canvas_x < cell['x'] + cell['width'] &&
-            canvas_y >= cell['y'] && canvas_y < cell['y'] + cell['height']) {
-
-            cell.color = selected_color;
-
-			fetch(`https://ahmetalper-reddit-place-clone.hf.space/update/${index}/${selected_color.substring(1)}`)
-
-				.then(response => response.json())
-
-				.then(data => {
-
-					console.log(`${new Date().toLocaleString()} | cell ${index} | ${selected_color} | ${data['status']}`);
-
-				})
-
-				.catch(error => {
-
-					console.error(`${new Date().toLocaleString()} | error : ${error}`);
-
-				});
-
-        }
-
-    });
-
-    draw_grid();
-
-});
-
-create_grid();
 
 draw_grid();
 
-update_grid();
-
-setInterval(update_grid, 10000);
+connect_websocket();
